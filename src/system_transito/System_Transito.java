0cb1a8067d921574333874d9bf8848335bf808dd
pmvj <pmvj2024@gmail.com>
2026-05-05 10:52:54 -0500

    PRIMER COMMIT DEL PROYECTO DE TRANSITO

---
+++ src/system_transito/System_Transito.java
@@ -0,0 +1,15 @@
+
+package system_transito;
+
+
+public class System_Transito {
+
+  
+    public static void main(String[] args) {
+        
+        
+        
+        System.out.println("Hola a todos");
+    }
+    
+}
